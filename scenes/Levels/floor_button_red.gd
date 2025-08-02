2f2051b8da533083fb0026fc09794e2c7b519ddb
Ayden McNew <mcnew1721@gmail.com>
2025-08-02 10:35:53 -0400

    added floor and wall sensors - sensor not working

---
+++ scenes/Levels/floor_button_red.gd
@@ -0,0 +1,25 @@
+extends CSGBox3D
+
+
+signal pressed
+signal released
+
+
+@export var pressed_y: float = 0.1
+@export var disabled: bool = false
+var down: bool = false
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if !down and body is not CSGPrimitive3D:
+		print(position)
+		position.y -= pressed_y
+		down = true
+		pressed.emit()
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if down and body is not CSGPrimitive3D:
+		print(position)
+		position.y += pressed_y
+		down = false
+		pressed.emit()
---
+++ scenes/Levels/wall_sensor_orange.gd
@@ -0,0 +1,37 @@
+extends CSGCylinder3D
+
+signal disrupted
+signal clear
+
+@export var disabled: bool = true
+@export var light: OmniLight3D
+@export var default_light_color: Color = Color(0.214, 0.404, 1.0)
+
+@export var button: Node3D
+
+func _ready() -> void:
+	button.connect("pressed",on_entity_signal_true)
+	button.connect("released",on_entity_signal_false)
+
+func _process(delta: float) -> void:
+	if !disabled:
+		light.light_color = Color(0.881, 0.0, 0.249)
+	else:
+		light.light_color = default_light_color
+		
+func on_entity_signal_true():
+	disabled = false	
+	
+func on_entity_signal_false():
+	disabled = true			
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if !disabled:
+		disrupted.emit()
+		light.light_color = Color(0.0, 0.651, 0.287)
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if !disabled:
+		clear.emit()
+		light.light_color = default_light_color
