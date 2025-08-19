aa8ae5077e44e26247839ae16f3d20a1bdbc05e4
Ayden McNew <mcnew1721@gmail.com>
2025-08-19 01:03:07 -0400

    began new level 2 - fixed edge WallEntity edge cases

--- scripts/Entities/ButtonEntity.gd
+++ scripts/Entities/ButtonEntity.gd
@@ -4,6 +4,7 @@ class_name ButtonEntity extends Entity
 @export var disabled: bool = false
 @export var area: Area3D
 var pressed: bool = false
+var current_bod:  CharacterBody3D
 
 func _ready() -> void:
 	if area:
@@ -11,7 +12,8 @@ func _ready() -> void:
 		area.body_exited.connect(_on_area_3d_body_exited)
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	if !pressed and body is not CSGPrimitive3D:
+	if !pressed and(body is Echo or body.name == "Player"):
+		current_bod = body
 		print(position)
 		position.y -= pressed_y
 		pressed = true
@@ -19,7 +21,8 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
-	if pressed and body is not CSGPrimitive3D:
+	if pressed and (body is Echo or body.name == "Player") and current_bod == body:
+		current_bod = null
 		print(position)
 		position.y += pressed_y
 		pressed = false
--- scripts/Entities/WallEntity.gd
+++ scripts/Entities/WallEntity.gd
@@ -2,12 +2,17 @@ class_name WallEntity extends Entity
 
 @export var entity: Entity
 @export var timer: Timer
-@export var up_y: float = 3.0
-var default_y: float
+@export var pos_change: Vector3 = Vector3(0,3,0)
+@export var tween_out: bool = false
+@export var on: bool = false
+var default_pos: Vector3
 
 
 func _ready() -> void:
-	default_y = position.y
+	default_pos = position
+	if on:
+		
+		on_entity_signal_true()
 	if entity != null:
 		entity.connected.connect(on_entity_signal_true)
 		entity.disconnected.connect(on_entity_signal_false)
@@ -16,16 +21,22 @@ func _ready() -> void:
 		timer.timeout.connect(on_entity_signal_false)
 
 func _process(delta: float) -> void:
-	if timer.is_stopped():
-		position.y = default_y
+	if timer:
+		if timer.is_stopped() and !on:
+			position = default_pos
 
 func on_entity_signal_true():
 	var tween = get_tree().create_tween()
-	var pos = Vector3(position.x,position.y + up_y,position.z)
+	var pos = position + pos_change
 	tween.tween_property(self,"position",pos,0.4)
 	
 		
 	
 func on_entity_signal_false():
-	position.y = default_y
+	if tween_out:
+		var tween = get_tree().create_tween()
+		
+		tween.tween_property(self,"position",default_pos,0.4)
+	else:
+		position = default_pos
 	disconnected.emit()
